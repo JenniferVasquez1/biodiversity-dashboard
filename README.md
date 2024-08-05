55d86eeee1ac81fd8abce2886000535acbe3897b
Jennifer Harris <jennifer.v.harris@outlook.com>
2024-08-05 14:31:20 -0400

    completed js file and readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # biodiversity-dashboard
+![MIT](https://img.shields.io/badge/License-MIT-blue)
+
+## Website: 
+[website](https://github.com/JenniferVasquez1/biodiversity-dashboard/tree/main)
+
+## Description
+Built an interative dashboard to explore the Belly Button Biodiversity data set which catalogs the microbes that colonize human navels. The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+![app_image](dashboard.png)
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+
+- [Contact](#contact)
+
+## Installation
+python, javascript, html, json
+
+## Usage
+run js file and html
+
+## Credits
+Jennifer Harris
+
+## License
+MIT
+
+## Features
+html website, bar chart, bubble chart
+
+
+
+## Contact
+If there are any questions of concerns, I can be reached at:
+##### [github: JenniferVasquez1](https://github.com/JenniferVasquez1)
+##### [email: jennifer.v.harris@outlook.com](mailto:jennifer.v.harris@outlook.com)
---
+++ static/js/app.js
@@ -0,0 +1,74 @@
+
+const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
+
+
+const optionChanged = async option => {
+
+  data = await d3.json(url);
+
+// Populating options in select element if option variable is undefined
+  if (option == undefined) {
+    data.names.forEach(name => {
+      d3.select('#selDataset').append('option').text(name);
+    });
+
+    const renderCharts = (option) => {
+      let meta = data.metadata.find(obj => obj.id == option);
+      let { otu_ids, sample_values, otu_labels } = data.samples.find(obj => obj.id == option);
+
+      // Demographic Info
+      d3.select('#sample-metadata').html('');
+      Object.entries(meta).forEach(([k, v]) => {
+        d3.select('#sample-metadata').append('h6').text(`${k.toUpperCase()}: ${v}`)
+      });
+
+      // Bar Chart Info
+      var graph_data = [
+        {
+          x: sample_values.slice(0, 10).reverse(),
+          y: otu_ids.map(x => `OTU ${x}`).slice(0, 10).reverse(),
+          text: otu_labels.slice(0, 10).reverse(),
+          type: 'bar',
+          orientation: 'h'
+        }
+      ];
+
+      layout = {
+        'title': 'Top 10 Bacteria Cultures Found',
+        xaxis: { title: 'Number of Bacteria' }
+      }
+
+      Plotly.newPlot('bar', graph_data, layout);
+
+      // Bubble Chart Info
+      var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        mode: 'markers',
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: 'Earth'
+        }
+      };
+
+      layout = {
+        title: 'Bacteria Cultures Per Sample',
+        xaxis: {title: 'OTU ID'},
+        yaxis: {title: 'Number of Bacteria'}
+      }
+      
+      var bubble_data = [trace1];
+      
+      Plotly.newPlot('bubble', bubble_data, layout);      
+    };
+
+
+    renderCharts(data.names[0])
+  };
+
+
+
+}
+
+optionChanged();
